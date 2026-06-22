797aee19675f2e82f6f849ab5511ab5cca5e746d
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-06-22 14:27:11 +0100

    setting up the raftmeta

---
+++ internal/node/raft_meta.go
@@ -0,0 +1,29 @@
+package internal
+
+import (
+	"os"
+
+	"go.etcd.io/raft/v3/raftpb"
+)
+
+type RaftMeta struct {
+	file   *os.File
+	path   string
+	nodeID uint64
+}
+
+func OpenRaftMeta(path string, nodeID uint64) (*RaftMeta, error) {
+
+}
+
+func (r *RaftMeta) Load() (raftpb.HardState, raftpb.ConfState, error) {
+
+}
+
+func (r *RaftMeta) Save(hs raftpb.HardState, cs raftpb.ConfState) error {
+
+}
+
+func (r *RaftMeta) Close() error {
+
+}
