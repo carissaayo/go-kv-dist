c4422bafe748e7ed6f5eb984406c9231db1b9df0
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-06-26 15:13:27 +0100

    utility functions to help track if the current node is now the leader

--- cmd/kvd/main.go
+++ cmd/kvd/main.go
@@ -29,7 +29,38 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
+	log.Printf("kvd: node %d running (phase 1); data=%s", *id, *dataDir)
+
+	if err := waitUntilLeader(ctx, n, *id); err != nil {
+		log.Fatalf("wait for leader: %v", err)
+	}
+
+	log.Printf("kvd: elected leader; status=%+v", n.Status())
+
+	lastBefore, err := n.Storage().LastIndex()
+	if err != nil {
+		log.Fatalf("last index: %v", err)
+	}
+
+	if err := n.Propose(ctx, []byte("noop")); err != nil {
+		log.Fatalf("propose: %v", err)
+	}
+
+	if err := waitUntilLogGrows(ctx, n, lastBefore); err != nil {
+		log.Fatalf("wait for log: %v", err)
+	}
+
+	last, err := n.Storage().LastIndex()
+	if err != nil {
+		log.Fatalf("last index: %v", err)
+	}
+
+	log.Printf("kvd: propose committed; lastIndex=%d status=%+v", last, n.Status())
+
+	log.Printf("kvd: restart with same --data to verify RestartNode path")
+
 	<-ctx.Done()
+
 	log.Println("kvd: shutting down")
 }
 
@@ -54,6 +85,7 @@ func waitUntilLeader(ctx context.Context, n *node.Node, id uint64) error {
 func waitUntilLogGrows(ctx context.Context, n *node.Node, prev uint64) error {
 	ticker := time.NewTicker(50 * time.Millisecond)
 	defer ticker.Stop()
+
 	for {
 		select {
 		case <-ctx.Done():
