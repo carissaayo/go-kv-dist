a6753da715ed0978e0fe298187a488bc17e5e98e
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-06-27 16:38:39 +0100

    boot order updated

--- cmd/kvd/main.go
+++ cmd/kvd/main.go
@@ -21,31 +21,36 @@ import (
 func main() {
 	id := flag.Uint64("id", 1, "raft node ID")
 	dataDir := flag.String("data", "./data/node1", "data directory")
-	listen := flag.String("listen", "localhost:50051", "gRPC listen address")
+	addr := flag.String("addr", "localhost:50051", "gRPC listen address (KV + raft transport)")
+	peers := flag.String("peers", "", "peer addresses: id=host:port,id=host:port (other nodes; self uses --addr)")
+	listen := flag.String("listen", "", "deprecated alias for --addr")
 	flag.Parse()
 
-	n, err := node.NewNode(*dataDir, *id)
+	listenAddr := *addr
+	if *listen != "" {
+		listenAddr = *listen
+	}
+
+	peerAddrs, err := node.ParsePeerAddrs(*id, listenAddr, *peers)
+	if err != nil {
+		log.Fatalf("parse peers: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatalf("listen: %v", err)
+	}
+
+	n, err := node.NewNode(*dataDir, *id, node.Options{PeerAddrs: peerAddrs})
 	if err != nil {
 		log.Fatalf("start node: %v", err)
 	}
 	defer n.Stop()
 
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer cancel()
-
-	if err := waitUntilLeader(ctx, n, *id); err != nil {
-		log.Fatalf("wait for leader: %v", err)
-	}
-	log.Printf("kvd: node %d leader; data=%s listen=%s", *id, *dataDir, *listen)
-
-	lis, err := net.Listen("tcp", *listen)
-	if err != nil {
-		log.Fatalf("listen: %v", err)
-	}
-
 	srv := grpc.NewServer()
 	pb.RegisterKVServer(srv, api.NewKVServer(n))
-	reflection.Register(srv) // enables grpcurl list/describe without -proto
+	node.RegisterRaftTransport(srv, n)
+	reflection.Register(srv)
 
 	go func() {
 		if err := srv.Serve(lis); err != nil {
@@ -54,11 +59,19 @@ func main() {
 	}()
 	defer srv.GracefulStop()
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	if err := waitForRaftReady(ctx, n); err != nil {
+		log.Fatalf("wait for raft: %v", err)
+	}
+	log.Printf("kvd: node %d up; leader=%d addr=%s data=%s", *id, n.LeaderID(), listenAddr, *dataDir)
+
 	<-ctx.Done()
 	log.Println("kvd: shutting down")
 }
 
-func waitUntilLeader(ctx context.Context, n *node.Node, id uint64) error {
+func waitForRaftReady(ctx context.Context, n *node.Node) error {
 	ticker := time.NewTicker(50 * time.Millisecond)
 	defer ticker.Stop()
 
@@ -67,7 +80,7 @@ func waitUntilLeader(ctx context.Context, n *node.Node, id uint64) error {
 		case <-ctx.Done():
 			return ctx.Err()
 		case <-ticker.C:
-			if n.Status().Lead == id {
+			if n.LeaderID() != 0 {
 				return nil
 			}
 		}
